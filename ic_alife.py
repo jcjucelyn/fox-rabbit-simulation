88dc631554e3eef1e3e06354eb8d055d08162754
macintosh air <macintoshair@MacBook-Pro.local>
2023-04-14 23:29:32 -0400

    comments and colors

--- ic_alife.py
+++ ic_alife.py
@@ -1,3 +1,10 @@
+"""
+Jocelyn Ju & Ceara Zhang
+Prof. John Rachlin
+HW05 : Predator Prey
+14 April 2023
+"""
+# import necessary libraries
 import random as rnd
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -9,14 +16,16 @@ from matplotlib import colors
 
 SIZE = 50  # The dimensions of the field
 fsize = 50
-# OFFSPRING = 2  # Max offspring when a rabbit reproduces
-GRASS_RATE = 0.025  # Probability that grass grows back at any location in the next season.
+OFFSPRING = 2  # Max offspring when a rabbit reproduces
+grass_rate = 0.025  # Probability that grass grows back at any location in the next season.
+# establish necessary constants
 WRAP = False  # Does the field wrap around on itself when rabbits move?
 
 
 class Animal:
-    """ A furry creature roaming a field in search of grass to eat.
-    Mr. Rabbit must eat enough to reproduce, otherwise he will starve. """
+    """ A creature in a field searching for something to eat. This animal
+     is adjustable with id, maximum offspring possible, speed at which it
+     moves, amount of cycles before it starves, and what it eats"""
 
     def __init__(self, id, max_offspring, speed, starve, eats, fsize):
         """
@@ -50,7 +59,7 @@ class Animal:
         return copy.deepcopy(self)
 
     def eat(self, amount):
-        """Feed the animal"""
+        """Feed the animal, taking in the amount"""
         self.eaten += amount
 
     def move(self):
@@ -59,6 +68,7 @@ class Animal:
         min_move = max_move * -1 + 1
         move_list = [i for i in range(min_move, max_move)]
 
+        # account for the field wrapping around
         if WRAP:
             self.x = (self.x + rnd.choice(move_list)) % self.fsize
             self.y = (self.y + rnd.choice(move_list)) % self.fsize
@@ -67,37 +77,47 @@ class Animal:
             self.y = min(self.fsize-1, max(0, (self.y + rnd.choice(move_list))))
 
     def survive(self):
-        # return whether the animal will survive that cycle
-        self.cycle += 1
-
+        """ Returns whether the animal will survive that cycle
+        taking into account the number of cycles the animal can
+        go without starving """
         # if the animal ate that cycle, reset counter to 0
         if self.eaten > 0:
             self.cycle = 0
             return True
 
         # if the animal has gone too many cycles without eating, return "False"
-        if self.cycle <= self.starve:
-            return True
         else:
-            return False
-        
+            if self.cycle <= self.starve:
+                self.cycle += 1
+                return True
+            else:
+                return False
 
 
 class Field:
-    """ A field is a patch of grass with 0 or more rabbits hopping around
-    in search of grass """
+    """ A field is a patch of grass with 0 or more rabbits and foxes
+     roaming around in search of food """
 
-    def __init__(self, size):
-        """ Create a patch of grass with dimensions SIZE x SIZE
-        and initially no rabbits """
+    def __init__(self, size, grass_rate):
+        """ Create a patch of grass with dimensions fsize x fsize
+        and initially no animals
+
+        animals : a list of the animals in the field
+        nrabbits: a list of the number of rabbits at each generation
+        nfoxes: a list of the number of foxes at each generation
+        ngrass: a list of the number of grass pixels at each generation
+        size: the size of the visualization
+        field: an array of none of size (size x size)
+        grass_rate: the probability that grass grows back at any given
+            location the next season
+        """
         self.animals = []
         self.nrabbits = []
-        # self.nfoxes = []
         self.nfoxes = []
         self.ngrass = []
         self.size = size
-        # self.field = np.full(shape=(self.size, self.size), fill_value=2, dtype=int)
         self.field = np.ones(shape=(self.size, self.size), dtype=int)
+        self.grass_rate = grass_rate
 
     def add_animal(self, animal):
         """ A new animal is added to the field """
@@ -110,22 +130,23 @@ class Field:
 
     def eat(self):
         """ Animals eat (if they find grass where they are) """
-        # for rabbit in self.rabbits:
-        #     rabbit.eat(self.field[rabbit.x, rabbit.y])
-        #     self.field[rabbit.x, rabbit.y] = 0
+
         for animal in self.animals:
             # check pixel of field's current value
+            # if it's edible, eat it and revert the field value to 0
             if self.field[animal.x, animal.y] in animal.eats:
                 animal.eat(self.field[animal.x, animal.y])
                 self.field[animal.x, animal.y] = 0
+            # if it is not, leave the field value as is
+            elif self.field[animal.x, animal.y] > animal.id:
+                self.field[animal.x, animal.y] = self.field[animal.x, animal.y]
 
     def survive(self):
-        """ Rabbits who eat some grass live to eat another day """
-        # self.rabbits = [r for r in self.rabbits if r.eaten > 0]
+        """ Animals who eat may live to eat another day, depending on their cycle survivals """
         self.animals = [r for r in self.animals if r.survive()]
 
     def reproduce(self):
-        """ Rabbits reproduce like rabbits. """
+        """ Animals reproduce according to their maximum offspring. """
         born = []
         for animal in self.animals:
             for _ in range(rnd.randint(1, animal.max_offspring)):
@@ -149,15 +170,17 @@ class Field:
         self.ngrass.append(self.amount_of_grass())
 
     def grow(self):
-        """ Grass grows back with some probability """
-        growloc = (np.random.rand(self.size, self.size) < GRASS_RATE) * 1
+        """ Grass grows back with some user-inputted probability """
+        growloc = (np.random.rand(self.size, self.size) < self.grass_rate) * 1
         self.field = np.maximum(self.field, growloc)
 
-    def get_animals(self):
-        """2D array where each element represents presence/absence of animal w/ ID"""
+    def get_animals(self, id):
+        """2D array where each element represents presence/absence
+        of animal according to their ID"""
         animals = np.zeros(shape=(self.size, self.size), dtype=int)
         for r in self.animals:
-            animals[r.x, r.y] = r.id
+            if r.id == id:
+                animals[r.x, r.y] = r.id
         return animals
 
     def num_animals(self):
@@ -168,11 +191,12 @@ class Field:
         for r in self.animals:
             num_dict[r.id] = []
 
+        # add the animal to its respective list in the dict
         for r in self.animals:
             num_dict[r.id] += [r]
 
+        # return the dictionary
         return num_dict
-        # return len([r for r in self.rabbits if r.id == 2])
 
     def amount_of_grass(self):
         """How much grass is there?"""
@@ -187,57 +211,83 @@ class Field:
         self.grow()
 
     def history(self, showTrack=True, showPercentage=True, marker='.'):
-        plt.figure(figsize=(6, 6))
-        plt.xlabel("# Rabbits")
-        plt.ylabel("# Grass")
+        """ Keep track of and store the history of the field as a png
+        Input:
+            showTrack (Boolean): whether to connect points or not
+            showPercentage (Boolean): whether to display the values of each animal
+                    in terms of percentage of the maximum values
+            marker (str): what marker to use for the plot
 
+        Return:
+            a plot of the number of animals and grass over time saved as a png
+        """
+        # set the size and labels of the plot
+        plt.figure(figsize=(6, 6))
+        plt.xlabel("Generations")
+        plt.ylabel("% Organisms")
+
+        # obtain the rabbits' scale through identifying the
+        # maximum value and setting it to 100%
         xs = self.nrabbits[:]
         if showPercentage:
             maxrabbit = max(xs)
             xs = [x / maxrabbit for x in xs]
-            plt.xlabel("% Rabbits")
 
+        # obtain the foxes' scale through identifying the
+        # maximum value and setting it to 100%
         xf = self.nfoxes[:]
         if showPercentage:
             maxfox = max(xf)
             xf = [x / maxfox for x in xf]
-            plt.xlabel("% Foxes")
 
+        # obtain the grass' scale through identifying the
+        # maximum value and setting it to 100%
         ys = self.ngrass[:]
         if showPercentage:
             maxgrass = max(ys)
             ys = [y / maxgrass for y in ys]
-            plt.ylabel("% Rabbits")
 
+        # create the x-axis of generations
         xli = [_ for _ in range(len(xf))]
 
+        # plot connected lines if showTrack
         if showTrack:
-            plt.plot(xli, xf, marker=marker, label='Foxes')
-            plt.plot(xli, xs, marker=marker, label='Rabbits')
-            plt.plot(xli, ys, marker=marker, label='Grass')
+            plt.plot(xli, xf, marker=marker)
+            plt.plot(xli, xs, marker=marker)
+            plt.plot(xli, ys, marker=marker)
+        # and a scatterplot if not showTrack
         else:
-            plt.scatter(xli, xf, marker=marker, label='Foxes')
-            plt.scatter(xli, xs, marker=marker, label='Rabbits')
-            plt.scatter(xli, ys, marker=marker, label='Grass')
+            plt.scatter(xli, xf, marker=marker)
+            plt.scatter(xli, xs, marker=marker)
+            plt.scatter(xli, ys, marker=marker)
 
         plt.grid()
 
-        plt.title("Foxes vs. Rabbits vs. Grass: GROW_RATE =" + str(GRASS_RATE))
-        plt.legend()
+        # add the title and legend to the plot
+        plt.title("Foxes vs. Rabbits vs. Grass: GROW_RATE =" + str(self.grass_rate))
+        plt.legend(['Foxes', 'Rabbits', 'Grass'])
         plt.savefig("history.png", bbox_inches='tight')
         plt.show()
 
 
 def animate(i, field, im):
+    """
+    Animate the field of rabbits and foxes
+    Input:
+        i (int): the iteration/generation
+        field (field): the field for which to simulate for
+        im (image): the image to output
 
-    while i <= 100:
+    Return:
+        im (image) an updated image
+    """
+    # stopping criterion of 1000 iterations
+    while i <= 1000:
         field.generation()
-        # print("AFTER: ", i, np.sum(field.field), len(field.rabbits))
+        # print("AFTER: ", i, np.sum(field.field), len(field.nfoxes))
         im.set_array(field.field)
         plt.title("generation = " + str(i))
         return im,
-
-
 
 
 # make a custom color map where
@@ -245,26 +295,14 @@ def animate(i, field, im):
 #       Grass      = green
 #       Rabbits    = blue
 #       Foxes      = red
-# ccm = colors.LinearSegmentedColormap.from_list('fox-rabbit-cm', ['white', 'red', 'blue', 'green'])
-# size = (fsize, fsize)
-# field = np.random.randint(0, 2, size, dtype=int)          # 1 = grass, 0 = bare earth
-# rabbits = np.random.randint(0, 2, size, dtype=int) * 2  # 2 = rabbit
-# foxes = np.random.randint(0, 2, size, dtype=int) * 3   # 3 = foxes
-#
-# total = np.maximum(field, np.maximum(rabbits, foxes))
-# print(field, "\n\n", rabbits, "\n\n", foxes, "\n\n", total)
-
-# clist = ['blue', 'red', 'white', 'green']
 clist = ['blue', 'red', 'white', 'green']
 my_cmap = colors.ListedColormap(clist)
 
-# plt.imshow(total, cmap=my_cmap, interpolation='none')
-# plt.show()
-
-
 
 def main():
+    # create a parser to support command-line arguments
     # parser = ap.ArgumentParser()
+    #
     # # add grass growth rate, fox k value, field size, num initial foxes and rabbits
     # parser.add_argument('grass_growth', type=float,
     #                     help='the probability that grass grows back at any location in the next season')
@@ -278,38 +316,48 @@ def main():
     #                     help='the starting amount of rabbits'),
     # args = parser.parse_args()
     #
+    # # assign variables to the inputs
     # grass_rate = args.grass_growth
     # fox_k = args.fox_k
     # fsize = args.field_size
     # init_fox = args.init_fox
     # init_rabbit = args.init_rabbit
     #
-    # Create the ecosystem
-    field = Field(fsize)
-
-    # # id, max_offspring, speed, starve, eats, fsize
-    # for _ in range(init_rabbit):
-    #     field.add_rabbit(Rabbit(2, 1, 1, 1, (1,), fsize))
+    # # Create the ecosystem
+    # field = Field(fsize, grass_rate)
     #
-    # # add fox
+    # # add rabbits (id, max_offspring, speed, starve, eats, fsize)
+    # for _ in range(init_rabbit):
+    #     field.add_animal(Animal(2, 1, 1, 1, (1,), fsize))
+    #
+    # # add foxes (id, max_offspring, speed, starve, eats, fsize)
+    # # with user inputs for fox_k and fsize
     # for _ in range(init_fox):
-    #     field.add_rabbit(Rabbit(3, 1, 1, fox_k, (2,), fsize))
+    #     field.add_animal(Animal(3, 1, 1, fox_k, (2,), fsize))
 
-    # add rabbit
+    # Create the ecosystem
+    field = Field(fsize, grass_rate)
 
+    # add rabbits
     for _ in range(1):
-        field.add_animal(Animal(2, 1, 1, 1, (1,), fsize))
+        field.add_animal(Animal(2, 2, 1, 1, (1,), fsize))
 
-    # add fox
-    for _ in range(10):
-        field.add_animal(Animal(3, 1, 2, 1, (2,), fsize))
+    # add foxes
+    for _ in range(20):
+        field.add_animal(Animal(3, 1, 2, 10, (2, ), fsize))
 
+    # create the initial array of grass (value = 1)
     array = np.ones(shape=(fsize, fsize), dtype=int)
-    fig = plt.figure(figsize=(5, 5))
-    im = plt.imshow(array, cmap=my_cmap, interpolation='hamming', aspect='auto', vmin=0.5, vmax=1)
-    anim = animation.FuncAnimation(fig, animate, fargs=(field, im,), frames=1000000, interval=1, repeat=True)
-    plt.show()
 
+    # plot the figure
+    fig = plt.figure(figsize=(5, 5))
+    im = plt.imshow(array, cmap=my_cmap, interpolation='none', aspect='auto', vmin=0, vmax=1)
+
+    # animate the figure
+    anim = animation.FuncAnimation(fig, animate, fargs=(field, im,), frames=1000000, interval=1, repeat=True)
+
+    # display the plot and save the history
+    plt.show()
     field.history()
 
 
@@ -320,4 +368,5 @@ if __name__ == '__main__':
 SOURCES:
 
 help with parsers- https://docs.python.org/3/library/argparse.html 
+help with command line- https://tacc.github.io/ctls2017/docs/intro_to_python/intro_to_python_101_argparse.html 
 """
